854d457063aecfc9a978b3e9a5a8344e74525c68
Jimmy P Taylor <jimmytaylor1279@gmail.com>
2023-09-27 12:37:05 -0500

    Consolidated files and synchronized with Bitbucket

---
+++ gui.py
@@ -0,0 +1,12 @@
+
+import tkinter as tk
+
+
+def setup_gui():
+    # Create the main application window
+    root = tk.Tk()
+
+    # TODO: Add widgets to the window
+
+    # Start the Tkinter event loop
+    root.mainloop()
---
+++ main.py
@@ -0,0 +1,14 @@
+
+import os
+import logging
+from gui import setup_gui
+
+
+def main():
+    setup_gui()
+    # TODO: Implement other functionalities
+    pass
+
+
+if __name__ == '__main__':
+    main()
