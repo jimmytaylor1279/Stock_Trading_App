d5f4ce88a72b66fc24e207446b3d45bc8f3b89c8
Jimmy P Taylor <jimmytaylor1279@gmail.com>
2023-11-06 15:52:46 -0600

    Added Start button to Gui

--- dataImporter.py
+++ dataImporter.py
@@ -1,26 +1,20 @@
 import pandas as pd
+import os
 
 class DataImporter:
     def __init__(self, symbol_utilities, api):
         self.symbol_utilities = symbol_utilities
         self.api = api  # Alpaca API object passed from Trader
 
-    def fetch_data(self):
+    def fetch_data_for_selected_symbol(self, selected_symbol):
         """
-        Fetch historical and real-time data for the most active stock symbols.
+        Fetch historical and real-time data for the selected stock symbol.
         """
-        # Get the most active stock symbols directly
-        most_active_symbols = self.symbol_utilities.fetch_most_active_symbols()
-
-        # Fetch historical data
-        historical_data = {}
-        real_time_data = {}
-        for symbol in most_active_symbols:
-            # Fetch historical data for the past 30 days
-            historical_data[symbol] = self.api.get_barset(symbol, 'day', limit=30).df[symbol]
-            
-            # Fetch real-time data (latest available)
-            real_time_data[symbol] = self.api.get_latest_trade(symbol)
+        # Fetch historical data for the past 30 days
+        historical_data = self.api.get_barset(selected_symbol, 'day', limit=30).df[selected_symbol]
+        
+        # Fetch real-time data (latest available)
+        real_time_data = self.api.get_latest_trade(selected_symbol)
 
         return historical_data, real_time_data
 
--- gui.py
+++ gui.py
@@ -1,26 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
-import subprocess
-import os
 
 class StockAppGUI:
     """
     Class to handle the GUI components of the Stock Trading App.
     """
 
-    def __init__(self, trader, symbol_utilities):
+    def __init__(self, trader, symbol_utilities, data_importer):
         """
         Initialize the GUI components.
         """
         self.trader = trader
         self.symbol_utilities = symbol_utilities
+        self.data_importer = data_importer
 
         self.master = tk.Tk()
         self.master.title("Stock Trading App")
         
         # Label to display stock price
-        self.stock_price_label = tk.Label(self.master, text="Stock Price:")
-        self.stock_price_label.grid(row=0, column=0)
+        self.stock_price_label = tk.Label(self.master, text="Stock Price: $0.00")
+        self.stock_price_label.grid(row=0, column=1)
         
         # Label to display stock symbol
         self.stock_symbol_label = tk.Label(self.master, text="Stock Symbol:")
@@ -30,14 +29,15 @@ class StockAppGUI:
         self.stock_symbol_var = tk.StringVar()
         self.stock_symbol_dropdown = ttk.Combobox(self.master, textvariable=self.stock_symbol_var)
         self.stock_symbol_dropdown.grid(row=1, column=1)
+        self.stock_symbol_dropdown.bind("<<ComboboxSelected>>", self.update_stock_price)
         
         # Button to update stock symbols
         self.update_symbols_button = tk.Button(self.master, text="Update Symbols", command=self.update_symbols)
         self.update_symbols_button.grid(row=2, column=0)
         
-        # Button to update dropdown values
-        self.update_dropdown_button = tk.Button(self.master, text="Update Dropdown", command=self.update_dropdown_values)
-        self.update_dropdown_button.grid(row=2, column=1)
+        # Button to start trading
+        self.start_trading_button = tk.Button(self.master, text="Start Trading", command=self.start_trading)
+        self.start_trading_button.grid(row=2, column=1)
         
         # Initialize dropdown values
         self.update_dropdown_values()
@@ -47,27 +47,44 @@ class StockAppGUI:
         Function to update trading symbols.
         """
         self.symbol_utilities.update_most_active_symbols()
-        
-        # Update the dropdown values after updating the symbols
         self.update_dropdown_values()
     
     def update_dropdown_values(self):
         """
         Function to update dropdown values.
         """
-        script_dir = os.path.dirname(os.path.abspath(__file__))  # Get the directory of this script
-        file_path = os.path.join(script_dir, "active_stocks.txt")  # Join it with the filename
-
-        with open(file_path, "r") as f:  # Use the full file path
-            symbols = f.readlines()
-        symbols = [symbol.strip() for symbol in symbols]  # Remove any trailing newlines
+        symbols = self.symbol_utilities.get_symbols_from_file()
         self.stock_symbol_dropdown['values'] = symbols
+        if symbols:
+            self.stock_symbol_dropdown.current(0)
+            self.update_stock_price(None)
 
+    def update_stock_price(self, event):
+        """
+        Function to update the stock price label for the selected symbol.
+        """
+        selected_symbol = self.stock_symbol_var.get()
+        if selected_symbol:
+            # Fetch the latest price for the selected symbol
+            price = self.trader.get_stock_price(selected_symbol)
+            self.stock_price_label.config(text=f"Stock Price: ${price:.2f}")
 
+    def start_trading(self):
+        """
+        Function to start trading the selected symbol.
+        """
+        selected_symbol = self.stock_symbol_var.get()
+        if selected_symbol:
+            # Fetch the 30-day history for the selected symbol
+            historical_data = self.data_importer.fetch_historical_data(selected_symbol)
+            # Start trading logic here
+            self.trader.start_trading(selected_symbol, historical_data)
 
 if __name__ == "__main__":
-    # For testing purposes, you can create mock instances of Trader and SymbolUtilities here
+    # For testing purposes, you can create mock instances of Trader, SymbolUtilities, and DataImporter here
     # trader = Trader()
     # symbol_utilities = SymbolUtilities()
-    # app = StockAppGUI(trader, symbol_utilities)
+    # data_importer = DataImporter()
+    # app = StockAppGUI(trader, symbol_utilities, data_importer)
+    # app.master.mainloop()
     pass
